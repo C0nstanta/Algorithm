293525287a58a8435e612d1d8babac5d85bab1c5
kokos <inter.nafta32@yahoo.com>
2022-05-18 17:07:32 +0300

    rebase three

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,7 @@
 int main(int argc, const char* argv[]){
   std::cout << "Hello world!" << std::endl;
   std::cout << "Bye bye" << std::endl;
-  std::cout << "Hello world again" << std::endl;
+  std::cout << "Hello world again" << std::endli;
+  std::cout << "Next rebase try." std::endl;
   return 0;
 }
